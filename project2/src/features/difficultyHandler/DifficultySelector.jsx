81496aff7a643c44b37bd1b915b8cb8549d3db8e
Yafei Wang <64439033+ricecakeM@users.noreply.github.com>
2022-03-31 23:41:51 -0700

    Add Emoji and style in DifficultySelector.jsx file
    Add Emoji and class name for styling in DifficultySelector.jsx file

--- project2/src/features/difficultyHandler/DifficultySelector.jsx
+++ project2/src/features/difficultyHandler/DifficultySelector.jsx
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import '../../App.css'
 
 export default function DifficultySelector() {
+  const Emoji = props => (
+    <span
+      className="emoji"
+      role="img"
+      aria-label={props.label ? props.label : ""}
+      aria-hidden={props.label ? "false" : "true"}
+    >
+      {props.symbol}
+    </span>
+  )
   return (
-    <div>
+    <div className='difficultySelector'>
           <div>
-          <div>
-            Please select the difficulty of the game.
-          </div>
-          <Link to="/Game/easy">
-            <button value="Game/easy">Easy</button>
-          </Link>
-          <Link to="/Game/medium">
-            <button value="Game/medium">Medium</button>
-          </Link>
-          <Link to="/Game/hard">
-            <button value="Game/hard">Hard</button>
-          </Link> 
+            <div className='game-instruction'>
+              Please select the difficulty of the game. 
+              Component: <Emoji label="happy" symbol="🥳"/>
+            </div>
+            <Link to="/Game/easy">
+              <button class="gradient-button" value="Game/easy">Easy</button>
+            </Link>
+            <Link class="gradient-button" to="/Game/medium">
+              <button value="Game/medium">Medium</button>
+            </Link>
+            <Link class="gradient-button" to="/Game/hard">
+              <button value="Game/hard">Hard</button>
+            </Link> 
         </div>
     </div>
   );
