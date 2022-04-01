563db8be2c1050487e488693c25984521f94007d
YuanYao-Ada <61042050+YuanYao-Ada@users.noreply.github.com>
2022-04-01 00:10:43 -0700

    Revert "Add Emoji and style in DifficultySelector.jsx file"

--- project2/src/features/difficultyHandler/DifficultySelector.jsx
+++ project2/src/features/difficultyHandler/DifficultySelector.jsx
@@ -1,34 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import '../../App.css'
 
 export default function DifficultySelector() {
-  const Emoji = props => (
-    <span
-      className="emoji"
-      role="img"
-      aria-label={props.label ? props.label : ""}
-      aria-hidden={props.label ? "false" : "true"}
-    >
-      {props.symbol}
-    </span>
-  )
   return (
-    <div className='difficultySelector'>
+    <div>
           <div>
-            <div className='game-instruction'>
-              Please select the difficulty of the game. 
-              Component: <Emoji label="happy" symbol="🥳"/>
-            </div>
-            <Link to="/Game/easy">
-              <button class="gradient-button" value="Game/easy">Easy</button>
-            </Link>
-            <Link class="gradient-button" to="/Game/medium">
-              <button value="Game/medium">Medium</button>
-            </Link>
-            <Link class="gradient-button" to="/Game/hard">
-              <button value="Game/hard">Hard</button>
-            </Link> 
+          <div>
+            Please select the difficulty of the game.
+          </div>
+          <Link to="/Game/easy">
+            <button value="Game/easy">Easy</button>
+          </Link>
+          <Link to="/Game/medium">
+            <button value="Game/medium">Medium</button>
+          </Link>
+          <Link to="/Game/hard">
+            <button value="Game/hard">Hard</button>
+          </Link> 
         </div>
     </div>
   );
